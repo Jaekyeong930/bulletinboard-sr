7c0e02ea1148a0843552306496e0791a7896ec4f
jkkim <jaekyeong930@gmail.com>
2023-05-19 01:51:16 +0900

    created Spring project and setted up

---
+++ BulletinBoardSR/src/main/java/jkkim/BulletinBoardSR/BulletinBoardSrApplication.java
@@ -0,0 +1,13 @@
+package jkkim.BulletinBoardSR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinBoardSrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulletinBoardSrApplication.class, args);
+	}
+
+}
---
+++ BulletinBoardSR/src/test/java/jkkim/BulletinBoardSR/BulletinBoardSrApplicationTests.java
@@ -0,0 +1,13 @@
+package jkkim.BulletinBoardSR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinBoardSrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
